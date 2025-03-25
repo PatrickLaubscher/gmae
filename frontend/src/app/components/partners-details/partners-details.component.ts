a208be23d51bf30e1e12d2f687ab42c48f3efd23
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-25 11:22:52 +0100

    installation front projet scrum

---
+++ frontend/src/app/components/partners-details/partners-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partners-details',
+  imports: [],
+  templateUrl: './partners-details.component.html',
+  styleUrl: './partners-details.component.css'
+})
+export class PartnersDetailsComponent {
+
+}
