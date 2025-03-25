373bf73b2ab3777676daa0c258eb5180c0e9dae1
Paul Lacoste <lacostepaul@yahoo.fr>
2025-03-25 15:00:31 +0100

    Auth Service : en cours

---
+++ frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly http = inject(HttpClient);
+
+  /**
+ * Vérifie si l'utilisateur est connecté
+ **/
+  login(pseudo: string, password: string): Observable<any> {
+    const payload = { pseudo, password };
+
+    // Appel à l'API d'authentification
+    return this.http.post('/api/login', payload).pipe(
+      map((response: any) => {
+        // Enregistre le token ds LocalStorage en cas de succès
+        localStorage.setItem('token', response.token);
+        return response;
+      }),
+      catchError((error) => {
+        console.error('Erreur de connexion', error);
+        throw error;
+      })
+    );
+  }
+
+  /**
+   * Vérifie si l'utilisateur est connecté
+   */
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
