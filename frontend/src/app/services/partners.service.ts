04942f8fce72a77d59a85b9f0a16e227d7c39568
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-25 17:34:54 +0100

    correction conflit

--- frontend/src/app/services/partners.service.ts
+++ frontend/src/app/services/partners.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../environments/environments';
+import {Partner} from './entities';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartnersService {
 
- 
 
-  constructor() { }
+  private baseUrl = environment.apiUrl + '/partners';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Partner[]> {
+    return this.http.get<Partner[]>(this.baseUrl);
+  }
+
+  getPartner(id: number): Observable<Partner> {
+    return this.http.get<Partner>(`${this.baseUrl}/${id}`);
+  }
+
+  createPartner(partnerData: Partner): Observable<Partner> {
+    return this.http.post<Partner>(this.baseUrl, partnerData);
+  }
+
+  updatePartner(id: number, partnerData: Partner): Observable<Partner> {
+    return this.http.put<Partner>(`${this.baseUrl}/${id}`, partnerData);
+  }
+
+  deletePartner(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
