94db8107d4bf5fceaf531d9588684a65142924d9
Pierre Beaufaron <p.beaufaron@gmail.com>
2025-03-27 09:54:09 +0100

    Template partner details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PartnersDetailsComponent } from './components/partners-details/partners-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'details', component: PartnersDetailsComponent },
 ];
 
