d6f45d960c294273c1b14eaa2745a2d84673bb4a
Timothée <83133135+timlapov@users.noreply.github.com>
2025-03-27 09:15:28 +0100

    Created interfaces for services, roles and users

--- frontend/src/app/services/entities.ts
+++ frontend/src/app/services/entities.ts
@@ -1,6 +1,29 @@
+export interface User {
+  id?: number;
+  email: string;
+  hash_password: string;
+  secret_question?: string;
+  roles: Role[];
+  nom: string;
+  prenom: string;
+}
+
+export interface Role {
+  id?: number;
+  name: string;
+  users?: User[];
+}
+
+export interface Service {
+  id?: number;
+  name: string;
+  partners?: Partner[];
+}
+
 export interface Partner {
   id?: number;
   name: string;
   description: string;
   logo: string;
+  categories?: Service[];
 }
